88683800a7ba9995f202e95e3c7167e66a7bf8ad
BenjaminFlatz <beni.flatz@gmail.com>
2021-06-03 14:48:27 +0200

    init

---
+++ lib/live_face_rec.py
@@ -0,0 +1,140 @@
+import time
+import face_recognition
+import cv2  # opencv-python
+import numpy as np
+import os
+import numpy as np
+from mss import mss
+
+
+class FaceRec:
+    def __init__(self, rec, scaleFactor, directory, width, height):
+        self.known_face_encodings = []
+        self.known_face_names = []
+
+
+        self.face_locations = []
+        self.face_names = []
+        self.face_encodings = []
+
+        self.process = True
+
+        self.rec = rec
+        self.scaleFactor = scaleFactor
+        self.width = width
+        self.height = height
+
+        self.get_2_know(directory)
+
+    def get_2_know(self, directory):
+
+        dictionary = {}
+
+        print(directory)
+
+        for item in os.listdir(directory):
+
+            print(item)
+            self.known_face_encodings.append(face_recognition.face_encodings(
+                face_recognition.load_image_file(directory + os.path.sep + item))[0])
+            self.known_face_names.append(item.split(".")[0])
+
+        return dictionary
+
+    def process_frame(self, frame):
+
+        if self.process:
+
+            self.face_locations = face_recognition.face_locations(frame)
+            self.face_encodings = face_recognition.face_encodings(frame, self.face_locations)
+
+            self.face_names = []
+            for face_encoding in self.face_encodings:
+             
+                matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                name = "Unknown"
+
+                # # If a match was found in self.known_face_encodings, just use the first one.
+                #
+                # if True in matches:
+                #     first_match_index = matches.index(True)
+                #     name = self.known_face_names[first_match_index]
+
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = self.known_face_names[best_match_index]
+
+                self.face_names.append(name)
+
+        self.process = not self.process
+
+
+    def get_frame(self, frame):
+        
+        last_time = time.time()
+        
+
+        small_frame = cv2.resize(frame, (0, 0), fx=self.scaleFactor, fy=self.scaleFactor)
+        small_frame = small_frame[:, :, ::-1]
+        
+        self.process_frame(small_frame)
+
+
+        for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+            top *= int(1/self.scaleFactor)
+            right *= int(1/self.scaleFactor)
+            bottom *= int(1/self.scaleFactor)
+            left *= int(1/self.scaleFactor)
+            cv2.rectangle(frame, (left, top),(right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, bottom - 35),(right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6),font, 1.0, (255, 255, 255), 1)
+        
+        print('fps: {0}'.format(1 / (time.time()-last_time)))
+        
+        return frame     
+
+    def rec_cam(self):
+        video_capture = cv2.VideoCapture(0)
+        process = True
+
+        while True:
+            ret, frame = video_capture.read()
+            cv2.imshow('Screen', self.get_frame(frame))
+            if cv2.waitKey(1) == ord("q"):
+                break
+
+        video_capture.release()
+        cv2.destroyAllWindows()
+            
+    def rec_screen(self):
+
+        mon = {'top': 0, 'left': 0, 'width': self.width, 'height': self.height}
+   
+        with mss() as sct:
+            while True:
+
+                frame = cv2.cvtColor(np.array(sct.grab(mon)), cv2.COLOR_BGRA2BGR)
+                cv2.imshow('Screen', self.get_frame(frame))
+
+                if cv2.waitKey(1) == ord("q"):
+                    break
+
+            cv2.destroyAllWindows()
+
+    def run(self):
+        
+        if self.rec == 'cam':
+            self.rec_cam()
+        elif self.rec == 'screen':
+            self.rec_screen()
+        else:
+            print('Choose a correct mode!')
+
+           
+
+
+
+
