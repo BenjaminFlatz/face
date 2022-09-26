40885c9c6be97ccfa0356230dc009d79f8d0d9d0
BenjaminFlatz <beni.flatz@gmail.com>
2022-09-26 20:20:17 +0000

    update

--- src/Upscale/Upscale.py
+++ src/Upscale/Upscale.py
@@ -5,6 +5,7 @@ from mss import mss
 import os
 import numpy as np
 import math
+import moviepy.editor as mp
 class Upscale:
     def __init__(self, modelPath, modelName, scaleFactor):
         self.modelPath = modelPath
@@ -12,7 +13,7 @@ class Upscale:
         self.scaleFactor = scaleFactor
 
         self.imageFileExtensions = (".jpg", ".jpeg", ".png", ".bmp")
-        self.videoFileExtensions = (".mp4")
+        self.videoFileExtensions = (".mp4", ".xvid", ".mkv")
         self.maxWidth = 500
         self.maxHeight = 500
 
@@ -24,80 +25,48 @@ class Upscale:
         self.sr.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
         self.sr.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
 
-    def UpscaleOversizedImage(self, image):
         
-        height, width, channels = image.shape
-        numberOfParts = math.ceil(width/self.maxWidth)
-        newImage = (0,0,0)
-        i = 0
-        height, width, channels = image.shape  
-        partWidth = width//numberOfParts
-        cut = width-partWidth
-        while i < numberOfParts:
-            part = image[:, cut:]
-            image = image[:, :cut]
-            cut -= partWidth
-            newImage += self.sr.upsample(part)
-            cv2.imshow('part', part)
-            cv2.waitKey(0)
-
-            i += 1
-        
-        return newImage
-      
-
-        
-    def SplitImage(self, image, numberOfParts):
-        i = 0
-        parts = []
-        height, width, channels = image.shape  
-        partWidth = width//numberOfParts
-        rightPartWidth = partWidth
-        while i < numberOfParts:
-            leftPartWidth = width-rightPartWidth
-            rightPartWidth += partWidth
-            part = image[:, leftPartWidth:rightPartWidth]
-            self.sr.upsample(part)
-            parts.append(part)
-        return parts
-
-        
-    def AddImages(self, leftPart, rightPart):
+    def AddImages(self, leftImage, rightImage):
+        ha,wa = leftImage.shape[:2]
+        hb,wb = rightImage.shape[:2]
+        max_height = np.max([ha, hb])
+        total_width = wa+wb
+        new_img = np.zeros(shape=(max_height, total_width, 3))
+        new_img[:ha,:wa]=leftImage
+        new_img[:hb,wa:wa+wb]=rightImage
+        return new_img
         return cv2.add(leftPart, rightPart)
 
-    def UpscaleVideo(self, capture, outputPath):
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter(outputPath, fourcc, 29.97, (1920,1080))
-
+    def UpscaleVideo(self, videoPath, outputPath):
+        capture = cv2.VideoCapture(videoPath)
+        fps = capture.get(cv2.CAP_PROP_FPS)
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter(outputPath, fourcc, fps, (1080, 1920))
+        
         while capture.isOpened():
-            ret, frame = capture.read()
-            if ret == True: 
-                upscaled = self.sr.upsample(frame)
+            grabbed, frame = capture.read()
+            if grabbed:
+                upscaled = self.sr.upsample((frame).astype(np.uint8))
                 out.write(upscaled)
-                cv2.imshow('upscaled', upscaled)
-                #bicubic = cv2.resize(frame, 
-                #    (upscaled.shape[1], upscaled.shape[0]),
-                #    interpolation=cv2.INTER_CUBIC)
-
-                #cv2.imshow("Original", frame)
-                #cv2.imshow("Bicubic", bicubic)
-                #cv2.imshow("Super Resolution", upscaled)
+                cv2.imshow('upscaled', frame)
             else:
                 break
 
             if cv2.waitKey(1) == ord("q"):
                 break
-        
+    
+
         capture.release()   
         out.release()
         cv2.destroyAllWindows()
+        return 
+
 
     def Image(self, imagePath: str, outputPath: str):
 
-
         if imagePath.endswith(self.imageFileExtensions):
             image = cv2.imread(imagePath)
-            result = self.UpscaleOversizedImage(image)
+            result = self.sr.upsample((image).astype(np.uint8))
             cv2.imwrite(outputPath, result)
 
 
@@ -108,7 +77,7 @@ class Upscale:
             while True:
 
                 frame = cv2.cvtColor(np.array(sct.grab(mon)), cv2.COLOR_BGRA2BGR)
-                upscaled = self.sr.upsample(frame)
+                upscaled = self.sr.upsample((frame).astype(np.uint8))
                 cv2.imshow('Screen', upscaled)
 
                 if cv2.waitKey(1) == ord("q"):
@@ -119,5 +88,8 @@ class Upscale:
     
     def Video(self, videoPath: str, outputPath: str):
         if videoPath.endswith(self.videoFileExtensions):
-            capture = cv2.VideoCapture(videoPath)
-            self.UpscaleVideo(capture, outputPath)
+            self.UpscaleVideo(videoPath, outputPath)
+            clip = mp.VideoFileClip(videoPath)
+            upscaledClip = mp.VideoFileClip(outputPath)
+            upscaledClip = upscaledClip.set_audio(clip.audio)
+            upscaledClip.write_videofile(outputPath)
--- src/UpscaleScript.py
+++ src/UpscaleScript.py
@@ -5,7 +5,7 @@ from Upscale.VideoUpscaleService import VideoUpscaleService
 
 if __name__ == '__main__':
     #image = ImageUpscaleService("images/test.jpg", "images/upscaled_lapsrn.jpg", "models/LapSRN_x4.pb", "lapsrn", 4)
-    image = ImageUpscaleService("images/oversize.jpeg", "images/upscaled_oversize_lapsrn.jpeg", "models/LapSRN_x4.pb", "lapsrn", 4)
+    #image = ImageUpscaleService("images/oversize.jpeg", "images/upscaled_oversize_lapsrn.jpeg", "models/LapSRN_x4.pb", "lapsrn", 4)
 
 
     #image = ImageUpscaleService("images/forest.jpeg", "images/forest_edsr4.jpeg","models/EDSR_x4.pb", "edsr", 4)
@@ -14,6 +14,8 @@ if __name__ == '__main__':
 
     #screen = ScreenUpscaleService("models/ESPCN_x4.pb", "espcn", 4)
     
-    #video = VideoUpscaleService("videos/test.mp4", "videos/upscaled_espcn_x4.mp4", "models/ESPCN_x4.pb", "espcn", 4)    
+    video = VideoUpscaleService("videos/test_hd.mkv", "videos/upscaled_espcn_x2.mkv", "models/ESPCN_x2.pb", "espcn", 2)
+    #video = VideoUpscaleService("videos/test.mp4", "videos/upscaled_espcn_x2.mp4", "models/ESPCN_x2.pb", "espcn", 2)    
+    
     #video = VideoUpscaleService("videos/test.mp4", "videos/upscaled_edsr_x4.mp4","models/EDSR_x4.pb", "edsr", 4)
-    #video = VideoUpscaleService("videos/test.mp4", "videos/upscaled_lapsrn_x4.mp4", "models/LapSRN_x4.pb", "lapsrn", 8)
+    #video = VideoUpscaleService("videos/test.mp4", "videos/upscaled_lapsrn_x4.mp4", "models/LapSRN_x4.pb", "lapsrn", 4)
